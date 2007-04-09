9b1e861ed5ff0bed90fd18d100663634d76e77aa
xue <xue@67889f5b-f455-0410-82f9-b33d71ad10f5>
2007-04-09 03:39:18 +0000

    3.1.0 beta release.

---
+++ tags/3.1.0b/demos/blog/protected/Pages/ErrorReport.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * ErrorReport class file
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.pradosoft.com/
+ * @copyright Copyright &copy; 2006 PradoSoft
+ * @license http://www.pradosoft.com/license/
+ * @version $Id$
+ */
+
+/**
+ * ErrorReport class
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.pradosoft.com/
+ * @copyright Copyright &copy; 2006 PradoSoft
+ * @license http://www.pradosoft.com/license/
+ */
+class ErrorReport extends BlogPage
+{
+	public function onLoad($param)
+	{
+		parent::onLoad($param);
+		$this->ErrorMessage->Text=$this->Application->SecurityManager->validateData(urldecode($this->Request['msg']));
+	}
+}
+
+?>
