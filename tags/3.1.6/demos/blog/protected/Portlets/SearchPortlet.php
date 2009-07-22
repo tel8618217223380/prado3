ad800c23f6994ba359a38c8249c53bcc7a0739ab
Christophe.Boulain@gmail.com <Christophe.Boulain@gmail.com@67889f5b-f455-0410-82f9-b33d71ad10f5>
2009-07-22 07:05:11 +0000

    3.1.6 release tag

---
+++ tags/3.1.6/demos/blog/protected/Portlets/SearchPortlet.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * SearchPortlet class file
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.pradosoft.com/
+ * @copyright Copyright &copy; 2006 PradoSoft
+ * @license http://www.pradosoft.com/license/
+ * @version $Id$
+ */
+
+Prado::using('Application.Portlets.Portlet');
+
+/**
+ * SearchPortlet class
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.pradosoft.com/
+ * @copyright Copyright &copy; 2006 PradoSoft
+ * @license http://www.pradosoft.com/license/
+ */
+class SearchPortlet extends Portlet
+{
+	public function onInit($param)
+	{
+		parent::onInit($param);
+		if(!$this->Page->IsPostBack && ($keyword=$this->Request['keyword'])!==null)
+			$this->Keyword->Text=$keyword;
+	}
+
+	public function search($sender,$param)
+	{
+		$keyword=$this->Keyword->Text;
+		$url=$this->Service->constructUrl('SearchPost',array('keyword'=>$keyword),false);
+		$this->Response->redirect($url);
+	}
+}
+
+?>
---
+++ tags/3.1.6/tests/unit/Caching/TAPCCacheTest.php
@@ -0,0 +1,82 @@
+<?php
+require_once dirname(__FILE__).'/../phpunit.php';
+
+Prado::using('System.Caching.TAPCCache');
+
+/**
+ * @package System.Caching
+ */
+class TAPCCacheTest extends PHPUnit_Framework_TestCase {
+
+	protected $app = null;
+	protected static $cache = null;
+
+	protected function setUp() {
+		if(!extension_loaded('apc')) {
+			self::markTestSkipped('The APC extension is not available');
+		} else {
+				$basePath = dirname(__FILE__).'/mockapp';
+				$runtimePath = $basePath.'/runtime';
+				if(!is_writable($runtimePath)) {
+					self::markTestSkipped("'$runtimePath' is writable");
+				}
+				try {
+					$this->app = new TApplication($basePath);
+					self::$cache = new TAPCCache();
+					self::$cache->init(null);
+				} catch(TConfigurationException $e) {
+					self::markTestSkipped($e->getMessage());
+				}
+				
+		}
+	}
+
+	protected function tearDown() {
+		$this->app = null;
+		$this->cache = null;
+	}
+
+	public function testInit() {
+		throw new PHPUnit_Framework_IncompleteTestError();
+	}
+	
+	public function testPrimaryCache() {
+		self::$cache->PrimaryCache = true;
+		self::assertEquals(true, self::$cache->PrimaryCache);
+		self::$cache->PrimaryCache = false;
+		self::assertEquals(false, self::$cache->PrimaryCache);
+	}
+	
+	public function testKeyPrefix() {
+		self::$cache->KeyPrefix = 'prefix';
+		self::assertEquals('prefix', self::$cache->KeyPrefix);
+	}
+	
+	public function testSetAndGet() {
+		self::$cache->set('key', 'value');
+		self::assertEquals('value', self::$cache->get('key'));
+	}
+	
+	public function testAdd() {
+		try {
+			self::$cache->add('anotherkey', 'value');
+		} catch(TNotSupportedException $e) {
+			self::markTestSkipped('apc_add is not supported');
+			return;
+		}
+		self::assertEquals('value', self::$cache->get('anotherkey'));
+	}
+	
+	public function testDelete() {
+		self::$cache->delete('key');
+		self::assertEquals(false, self::$cache->get('key'));
+	}
+	
+	public function testFlush() {
+		$this->testSetAndGet();
+		self::assertEquals(true, self::$cache->flush());
+	}
+
+}
+
+?>
